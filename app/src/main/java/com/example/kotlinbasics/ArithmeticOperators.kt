26f05f880582097027368db7f1f2cc00d5511c3c
Manohar <manoharacharya63@gmail.com>
2023-03-07 23:56:04 +0530

    3rd ArithmeticOperators

---
+++ app/src/main/java/com/example/kotlinbasics/ArithmeticOperators.kt
@@ -0,0 +1,24 @@
+package com.example.kotlinbasics
+//Arithmetic Operators (| + | - | * | / | % |)
+fun main(){
+
+    var result1 = 5 + 3
+    println(result1)
+    var result2 = 5 - 3
+    println(result2)
+    var result3 = 5 * 3
+    println(result3)
+    var result4 = 15 / 3
+    println(result4)
+    var result5 = 15 % 3
+    println(result5)
+
+    var result6 = 5 + 3
+    result6 *= 2
+    println(result6)
+
+    val a = 5.0
+    val b = 3
+    val result7: Double = (a / b)
+    println(result7)
+}

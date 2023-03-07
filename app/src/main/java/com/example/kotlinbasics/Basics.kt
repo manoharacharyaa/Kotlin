dbf9f6034a49e52bba3e5df6125d6cb783bb9efb
Manohar <manoharacharya63@gmail.com>
2023-03-07 22:08:18 +0530

    1st var & val

---
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinbasics
+// var & val
+fun main(){
+    var myName = "Manohar"
+    myName = "Virat"
+    println("Hello " + myName)
+
+    val mySurname = "Acharya"
+    mySurname = "Kohli"
+    println("Hello " + mySurname)
+}
+
+//Diff between var & val
+//var can be overwritten
+//val can't be overwritten
+

cdefe7fc6fb48eaafb14956d14269a123f93a022
Manohar <manoharacharya63@gmail.com>
2023-03-08 08:16:37 +0530

    4th ComparisionOperators

---
+++ app/src/main/java/com/example/kotlinbasics/ComparisionOperator.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinbasics
+//ComparisonOperator (|==|!=|<|>|<=|>=|)
+fun main(){
+    val isEqual = 5==3
+    println("isEqual is $isEqual")
+
+    val isNotEqual = 5!=5
+    println("isNotEqual is $isNotEqual")
+
+    println("is-5greater3 ${-5>3}")
+
+    println("isLowerEqual3 ${5<=3}")
+}
